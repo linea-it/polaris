74cac7f787b7a68d2e3272b5e84518bdb1565bab
Eric Freire <ericfreire@gmail.com>
2019-05-17 12:49:00 -0300

    Padrão de layout e funcionalidades (#3)
    * Padrão de layout e funcionalidades
    
    * Add columns saved and published
    
    * Correção no teste automatizado e alteração do loding
    
    * Add sorting
    
    * Alteração no status

--- src/components/TableRunning.js
+++ src/components/TableRunning.js
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import PropTypes from 'prop-types';
 import Paper from '@material-ui/core/Paper';
+import Icon from '@material-ui/core/Icon';
 import InputLabel from '@material-ui/core/InputLabel';
 import Input from '@material-ui/core/Input';
 import MenuItem from '@material-ui/core/MenuItem';
@@ -12,6 +13,7 @@ import {
   SortingState,
   CustomPaging,
   SearchState,
+  SelectionState,
 } from '@devexpress/dx-react-grid';
 import {
   Grid,
@@ -21,6 +23,9 @@ import {
   TableColumnResizing,
   Toolbar,
   SearchPanel,
+  TableSelection,
+  TableColumnVisibility,
+  ColumnChooser,
 } from '@devexpress/dx-react-grid-material-ui';
 
 import CircularProgress from '@material-ui/core/CircularProgress';
@@ -75,6 +80,16 @@ const styles = {
   },
 };
 
+const tableHeaderRowCell = ({ ...restProps }) => (
+  <TableHeaderRow.Cell
+    {...restProps}
+    style={{
+      color: '#555555',
+      fontSize: '1em',
+    }}
+  />
+);
+
 class TableMyProcesses extends React.PureComponent {
   constructor(props) {
     super(props);
@@ -84,37 +99,35 @@ class TableMyProcesses extends React.PureComponent {
   get initialState() {
     return {
       columns: [
-        { name: 'process_id', title: 'Process ID' },
-        { name: 'start_time', title: 'Start Time' },
-        { name: 'end_time', title: 'End Time' },
+        { name: 'processes_process_id', title: 'Process ID' },
+        { name: 'processes_start_time', title: 'Start Time' },
+        { name: 'processes_end_time', title: 'End Time' },
         { name: 'duration', title: 'Duration' },
         { name: 'name', title: 'Pipeline' },
         { name: 'instance', title: 'Instance' },
         { name: 'release', title: 'Release' },
         { name: 'dataset', title: 'Dataset' },
         { name: 'owner', title: 'Owner' },
-        { name: 'status_id', title: 'Status' },
+        { name: 'processes_status_id', title: 'Status' },
+        { name: 'saved', title: 'Saved' },
+        { name: 'processes_flag_published', title: 'Published' },
       ],
-      defaultColumnWidths: [
-        { columnName: 'process_id', width: 140 },
-        { columnName: 'start_time', width: 190 },
-        { columnName: 'end_time', width: 190 },
-        { columnName: 'duration', width: 140 },
-        { columnName: 'name', width: 210 },
-        { columnName: 'instance', width: 140 },
-        { columnName: 'release', width: 140 },
-        { columnName: 'dataset', width: 180 },
-        { columnName: 'owner', width: 200 },
-        { columnName: 'status_id', width: 140 },
+      tableColumnExtensions: [
+        { columnName: 'processes_process_id', width: 140 },
+        { columnName: 'duration', width: 110 },
+        { columnName: 'processes_status_id', width: 110 },
+        { columnName: 'saved', width: 100 },
+        { columnName: 'processes_flag_published', width: 130 },
       ],
       data: [],
-      sorting: [{ columnName: 'process_id', direction: 'desc' }],
+      sorting: [{ columnName: 'processes_process_id', direction: 'desc' }],
       totalCount: 0,
       pageSize: 10,
       pageSizes: [5, 10, 15],
       currentPage: 0,
       loading: true,
       after: '',
+      selection: [],
       filter: 'all',
       searchValue: '',
     };
@@ -177,6 +190,35 @@ class TableMyProcesses extends React.PureComponent {
     );
   };
 
+  handleSelection = selected => {
+    this.setState({ selected: selected });
+  };
+
+  changeSelection = selection => {
+    // Neste caso a selecao e para uma linha apenas,
+    var selected_id, selectedRow;
+    if (selection.length > 0) {
+      // comparar a selecao atual com a anterior para descobrir qual
+      // linha foi selecionado por ultimo
+      const diff = selection.filter(x => !this.state.selection.includes(x));
+
+      selection = diff;
+      selected_id = diff[0];
+      selectedRow = this.state.data[selected_id];
+    } else {
+      selection = [];
+      selectedRow = null;
+    }
+
+    this.setState(
+      {
+        selection,
+        selectedRow,
+      },
+      this.handleSelection(selectedRow)
+    );
+  };
+
   clearData = () => {
     this.setState({
       data: [],
@@ -203,15 +245,17 @@ class TableMyProcesses extends React.PureComponent {
       const processesListLocal = processesList.processesList.edges.map(row => {
         const startTime = moment(row.node.startTime);
         const endTime = moment(row.node.endTime);
+        const diff = endTime.diff(startTime);
+        const duration = moment.utc(diff).format('HH:mm:ss');
 
         return {
-          process_id: row.node.processId,
-          start_time: row.node.startTime !== null ? row.node.startTime : '-',
-          end_time: row.node.endTime !== null ? row.node.endTime : '-',
+          processes_process_id: row.node.processId,
+          processes_start_time:
+            row.node.startTime !== null ? row.node.startTime : '-',
+          processes_end_time:
+            row.node.endTime !== null ? row.node.endTime : '-',
           duration:
-            row.node.startTime && row.node.endTime !== null
-              ? moment(endTime.diff(startTime)).format('hh:mm:ss')
-              : '-',
+            row.node.startTime && row.node.endTime !== null ? duration : '-',
           name: row.node.name,
           instance: row.node.instance,
           release:
@@ -227,7 +271,9 @@ class TableMyProcesses extends React.PureComponent {
                 })
               : '-',
           owner: row.node.session.user.displayName,
-          status_id: row.node.processStatus.name,
+          processes_status_id: row.node.processStatus.name,
+          saved: row.node.savedProcesses,
+          processes_flag_published: row.node.flagPublished,
         };
       });
       this.setState({
@@ -255,26 +301,151 @@ class TableMyProcesses extends React.PureComponent {
     this.setState(initialState, () => this.loadData());
   };
 
+  renderProcessesId = rowData => {
+    if (rowData.processes_process_id) {
+      return (
+        <span title={rowData.processes_process_id}>
+          {rowData.processes_process_id}
+        </span>
+      );
+    } else {
+      return '-';
+    }
+  };
+
+  renderStartTime = rowData => {
+    if (rowData.processes_start_time) {
+      return (
+        <span title={rowData.processes_start_time}>
+          {rowData.processes_start_time}
+        </span>
+      );
+    } else {
+      return '-';
+    }
+  };
+
+  renderEndTime = rowData => {
+    if (rowData.processes_end_time) {
+      return (
+        <span title={rowData.processes_end_time}>
+          {rowData.processes_end_time}
+        </span>
+      );
+    } else {
+      return '-';
+    }
+  };
+
+  renderDuration = rowData => {
+    if (rowData.duration) {
+      return <span title={rowData.duration}>{rowData.duration}</span>;
+    } else {
+      return '-';
+    }
+  };
+
+  renderName = rowData => {
+    if (rowData.name) {
+      return <span title={rowData.name}>{rowData.name}</span>;
+    } else {
+      return '-';
+    }
+  };
+
+  renderInstance = rowData => {
+    if (rowData.instance) {
+      return <span title={rowData.instance}>{rowData.instance}</span>;
+    } else {
+      return '-';
+    }
+  };
+
+  renderRelease = rowData => {
+    if (rowData.release) {
+      return <span title={rowData.release}>{rowData.release}</span>;
+    } else {
+      return '-';
+    }
+  };
+
+  renderDataset = rowData => {
+    if (rowData.dataset) {
+      return <span title={rowData.dataset}>{rowData.dataset}</span>;
+    } else {
+      return '-';
+    }
+  };
+
+  renderOwner = rowData => {
+    if (rowData.owner) {
+      return <span title={rowData.owner}>{rowData.owner}</span>;
+    } else {
+      return '-';
+    }
+  };
+
   renderStatus = rowData => {
     const { classes } = this.props;
-    if (rowData.status_id === 'failure') {
+    if (rowData.processes_status_id === 'failure') {
       return (
-        <span className={classes.btn} style={styles.btnFailure}>
+        <span
+          className={classes.btn}
+          style={styles.btnFailure}
+          title={rowData.processes_status_id}
+        >
           Failure
         </span>
       );
-    } else if (rowData.status_id === 'running') {
+    } else if (rowData.processes_status_id === 'running') {
       return (
-        <span className={classes.btn} style={styles.btnRunning}>
+        <span
+          className={classes.btn}
+          style={styles.btnRunning}
+          title={rowData.processes_status_id}
+        >
           Running
         </span>
       );
     } else {
       return (
-        <span className={classes.btn} style={styles.btnSuccess}>
+        <span
+          className={classes.btn}
+          style={styles.btnSuccess}
+          title={rowData.processes_status_id}
+        >
           Success
         </span>
       );
+    }
+  };
+
+  renderSaved = rowData => {
+    const { classes } = this.props;
+
+    if (rowData.saved) {
+      if (rowData.saved.savedDateEnd === null) {
+        return (
+          <CircularProgress
+            disableShrink
+            style={{ width: '25px', height: '25px' }}
+          />
+        );
+      } else {
+        return <Icon className={classes.iconCheck}>check</Icon>;
+      }
+    } else if (rowData.saved === null) {
+      return '-';
+    }
+  };
+
+  renderCheck = rowData => {
+    const { classes } = this.props;
+
+    if (rowData.processes_flag_published) {
+      return <Icon className={classes.iconCheck}>check</Icon>;
+    } else {
+      return '-';
     }
   };
 
@@ -322,7 +493,8 @@ class TableMyProcesses extends React.PureComponent {
       pageSizes,
       currentPage,
       totalCount,
-      defaultColumnWidths,
+      tableColumnExtensions,
+      selection,
     } = this.state;
 
     return (
@@ -337,6 +509,7 @@ class TableMyProcesses extends React.PureComponent {
             { columnName: 'release', sortingEnabled: false },
             { columnName: 'dataset', sortingEnabled: false },
             { columnName: 'owner', sortingEnabled: false },
+            { columnName: 'saved', sortingEnabled: false },
           ]}
         />
         <PagingState
@@ -346,22 +519,47 @@ class TableMyProcesses extends React.PureComponent {
           onPageSizeChange={this.changePageSize}
         />
         <CustomPaging totalCount={totalCount} />
-        <Table />
-        <TableColumnResizing defaultColumnWidths={defaultColumnWidths} />
-        <TableHeaderRow showSortingControls />
+        <SelectionState
+          selection={selection}
+          onSelectionChange={this.changeSelection}
+        />
+        <Table columnExtensions={tableColumnExtensions} />
+        <TableColumnResizing />
+        <TableHeaderRow
+          cellComponent={tableHeaderRowCell}
+          showSortingControls
+        />
+        <TableColumnVisibility />
+        <TableSelection
+          selectByRowClick
+          highlightRow
+          showSelectionColumn={false}
+        />
         <PagingPanel pageSizes={pageSizes} />
         <Toolbar />
         <SearchPanel />
+        <ColumnChooser />
       </Grid>
     );
   };
 
   render() {
-    const { data, loading } = this.state;
+    const { loading, data } = this.state;
     const { classes } = this.props;
 
     data.map(row => {
-      row.status_id = this.renderStatus(row);
+      row.processes_process_id = this.renderProcessesId(row);
+      row.processes_start_time = this.renderStartTime(row);
+      row.processes_end_time = this.renderEndTime(row);
+      row.duration = this.renderDuration(row);
+      row.name = this.renderName(row);
+      row.instance = this.renderInstance(row);
+      row.release = this.renderRelease(row);
+      row.dataset = this.renderDataset(row);
+      row.owner = this.renderOwner(row);
+      row.processes_status_id = this.renderStatus(row);
+      row.saved = this.renderSaved(row);
+      row.processes_flag_published = this.renderCheck(row);
       return row;
     });
 
