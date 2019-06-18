a4e4b8b2426caf95ae078d12508ce9890496da0b
Cristiano Singulani <crisingulani@gmail.com>
2019-06-17 21:49:45 -0300

    Dynamic footer has been implemented. (#7)
    * Dynamic footer has been implemented.
    
    * Added new REACT_APP_INSTANCE config
    
    * Version removed
    
    * Improvemented in html render
    
    * Footer improvemented
    
    * fix lint

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,6 +4,7 @@ import { withStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import { Typography, Toolbar } from '@material-ui/core';
 import logo from '../assets/img/linea-logo-mini.png';
+import git_json from '../assets/json/version.json';
 
 const styles = {
   grow: {
@@ -17,6 +18,12 @@ const styles = {
     alignItems: 'center',
     justifyContent: 'space-between',
   },
+  versionLink: {
+    color: '#d2cf00',
+    textDecoration: 'none',
+    fontSize: '0.9rem',
+    cursor: 'pointer',
+  },
 };
 
 class Footer extends Component {
@@ -28,14 +35,48 @@ class Footer extends Component {
     window.open('http://www.linea.gov.br/', 'linea');
   };
 
+  openGithub = vlink => {
+    if (vlink) {
+      window.open(vlink);
+    }
+  };
+
   render() {
     const { classes } = this.props;
+
+    const instance =
+      process.env.NODE_ENV === 'production'
+        ? window._env_.REACT_APP_INSTANCE
+        : process.env.REACT_APP_INSTANCE;
+
+    let version = '--';
+    let vlink = null;
+
+    if (
+      Object.entries(git_json).length !== 0 &&
+      git_json.constructor === Object
+    ) {
+      version = git_json.version
+        ? `${git_json.version}`
+        : `${git_json.git.sha1.substring(0, 10)}`;
+
+      vlink = `${git_json.git.repository.replace(/.git$/, '')}/commit/${
+        git_json.git.sha1
+      }`;
+    }
+
     return (
       <footer className={classes.root}>
         <AppBar position="fixed" color="primary" className={classes.appBar}>
           <Toolbar className={classes.toolbar}>
-            <Typography className={classes.grow} color="inherit">
-              Developer Portal Instance
+            <Typography color="inherit" className={classes.grow}>
+              {instance} Portal:{' '}
+              <span
+                onClick={() => this.openGithub(vlink)}
+                className={classes.versionLink}
+              >
+                {version}
+              </span>
             </Typography>
             <Typography color="inherit">Powered by</Typography>
             <img
