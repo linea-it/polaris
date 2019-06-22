42cb21e634e83da73bd5ab843ccbbbf044abbbde
Cristiano Singulani <crisingulani@gmail.com>
2019-06-22 10:41:27 -0300

    Debbuging Jenkins error

--- version.sh
+++ version.sh
@@ -2,10 +2,6 @@
 
 GIT_TAG="$(git describe --tags ${GIT_COMMIT})"
 
-echo "$(git describe --tags ${GIT_COMMIT})"
-
-printenv
-
 cat >./src/assets/json/version.json <<EOF
     {
         "tag": "$GIT_TAG",
