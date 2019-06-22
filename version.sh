fabfb4d2645155c131c5da35fd1e9fe9a33bbc48
Cristiano Singulani <crisingulani@gmail.com>
2019-06-22 13:28:34 -0300

    Version error (#13)
    * Fixed error in version.sh shell script.
    
    * Fixed error in version.sh
    
    * Debbuging error in Jenkins
    
    * Debbuging Jenkins error
    
    * Debbuging version.sh
    
    * Debbuging version.sh

--- version.sh
+++ version.sh
@@ -4,6 +4,8 @@ echo "Executing: git describe --tags ${GIT_COMMIT}"
 export GIT_TAG="$(git describe --tags ${GIT_COMMIT})"
 echo "TAG: ${GIT_TAG}"
 
+printenv
+
 cat >./src/assets/json/version.json <<EOF
     {
         "tag": "$GIT_TAG",
