e76e47e498e5a14aa27ccb810be4932c685f3fd7
Matheus Freitas <mtsallein@gmail.com>
2019-06-21 22:50:14 -0300

    Correção do erro de filtragem (#8)
    * Correção do erro de filtragem
    
    * fix lint error

--- src/components/TableRunning.js
+++ src/components/TableRunning.js
@@ -110,7 +110,7 @@ class TableMyProcesses extends React.PureComponent {
         { name: 'tguser_display_name', title: 'Owner' },
         { name: 'processstatus_display_name', title: 'Status' },
         { name: 'saved', title: 'Saved' },
-        { name: 'processes_flag_published', title: 'Published' },
+        { name: 'processes_published_date', title: 'Published' },
       ],
       defaultColumnWidths: [
         { columnName: 'processes_process_id', width: 140 },
@@ -124,7 +124,7 @@ class TableMyProcesses extends React.PureComponent {
         { columnName: 'tguser_display_name', width: 180 },
         { columnName: 'processstatus_display_name', width: 110 },
         { columnName: 'saved', width: 100 },
-        { columnName: 'processes_flag_published', width: 130 },
+        { columnName: 'processes_published_date', width: 130 },
       ],
       data: [],
       sorting: [{ columnName: 'processes_process_id', direction: 'desc' }],
@@ -284,7 +284,7 @@ class TableMyProcesses extends React.PureComponent {
           tguser_display_name: row.node.session.user.displayName,
           processstatus_display_name: row.node.processStatus.name,
           saved: row.node.savedProcesses,
-          processes_flag_published: row.node.publishedDate,
+          processes_published_date: row.node.publishedDate,
         };
       });
       this.setState({
@@ -451,9 +451,10 @@ class TableMyProcesses extends React.PureComponent {
 
   renderSaved = rowData => {
     const { classes } = this.props;
-
-    if (rowData.saved) {
-      const tooltDate = rowData.saved.savedDateEnd.split('T')[0];
+    if (rowData.saved && rowData.saved.savedDateEnd) {
+      const tooltDate = moment
+        .utc(rowData.saved.savedDateEnd)
+        .format('YYYY-MM-DD');
       if (rowData.saved.savedDateEnd === null) {
         return (
           <CircularProgress
@@ -475,9 +476,10 @@ class TableMyProcesses extends React.PureComponent {
 
   renderCheck = rowData => {
     const { classes } = this.props;
-
-    if (rowData.processes_flag_published) {
-      const publishedDate = rowData.processes_flag_published.split('T')[0];
+    if (rowData.processes_published_date) {
+      const publishedDate = moment
+        .utc(rowData.processes_published_date)
+        .format('YYYY-MM-DD');
       return (
         <Icon title={publishedDate} className={classes.iconCheck}>
           check
@@ -487,7 +489,6 @@ class TableMyProcesses extends React.PureComponent {
       return '-';
     }
   };
-
   renderLoading = () => {
     return (
       <CircularProgress
@@ -599,7 +600,7 @@ class TableMyProcesses extends React.PureComponent {
       row.tguser_display_name = this.renderOwner(row);
       row.processstatus_display_name = this.renderStatus(row);
       row.saved = this.renderSaved(row);
-      row.processes_flag_published = this.renderCheck(row);
+      row.processes_published_date = this.renderCheck(row);
       return row;
     });
 
