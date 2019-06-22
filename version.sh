125d93d323d6ceaf5c5efa665895e77d0161eff3
Cristiano Singulani <crisingulani@gmail.com>
2019-06-21 23:05:03 -0300

    Footer (#9)
    * Dynamic footer has been implemented.
    
    * Added new REACT_APP_INSTANCE config
    
    * Version removed
    
    * Improvemented in html render
    
    * Footer improvemented
    
    * fix lint
    
    * fix .env
    
    * Improvements in obtaining the version.
    
    * Fixed path error
    
    * Fixed lint error

---
+++ version.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+GIT_TAG=$(`git describe --tags ${GIT_COMMIT}`)
+
+cat >./src/assets/json/version.json <<EOF
+    {
+        "tag": "$GIT_TAG",
+        "sha": "$GIT_COMMIT",
+        "url": "$GIT_URL"
+    }
+EOF
