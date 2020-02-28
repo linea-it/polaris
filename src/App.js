12d26adf11babf0ea6923f0b621c70ae395c8e4e
Matheus Allein <mtsallein@gmail.com>
2020-02-27 23:17:14 -0300

    Persisting state in local storage

--- src/App.js
+++ src/App.js
@@ -3,9 +3,10 @@ import { MuiThemeProvider } from '@material-ui/core/styles';
 import light from './themes/light';
 import dark from './themes/dark';
 import Home from './views/home/home';
+import useLocalState from './utils/useLocalState';
 
 function App() {
-  const [theme, setTheme] = useState('light');
+  const [theme, setTheme] = useLocalState('theme', 'light');
 
   const handleThemeSwitch = () =>
     setTheme(theme === 'light' ? 'dark' : 'light');
