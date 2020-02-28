7a93e8d6c728bee416d070b7315001362a2d075f
Matheus Allein <mtsallein@gmail.com>
2020-02-27 23:07:02 -0300

    Adding dark theme and a theme switcher

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { MuiThemeProvider } from '@material-ui/core/styles';
-import theme from './theme/MaterialTheme';
+import light from './themes/light';
+import dark from './themes/dark';
 import Home from './views/home/home';
 
 function App() {
+  const [theme, setTheme] = useState('light');
+
+  const handleThemeSwitch = () =>
+    setTheme(theme === 'light' ? 'dark' : 'light');
+
   return (
-    <MuiThemeProvider theme={theme}>
-      <Home />
+    <MuiThemeProvider theme={theme === 'light' ? light : dark}>
+      <Home theme={theme} handleThemeSwitch={handleThemeSwitch} />
     </MuiThemeProvider>
   );
 }
